dfa09a3bc4df9284284f93dadb33d0f6380e011b
UjalaTripathy <151170744+UjalaTripathy@users.noreply.github.com>
2023-11-17 19:22:46 +0530

    Update NewTest.java

--- src/application/NewTest.java
+++ src/application/NewTest.java
@@ -1,19 +1,1 @@
-package application;
 
-import org.testng.annotations.Test;
-import org.testng.annotations.BeforeTest;
-import org.testng.annotations.AfterTest;
-
-public class NewTest {
-  @Test
-  public void f() {
-  }
-  @BeforeTest
-  public void beforeTest() {
-  }
-
-  @AfterTest
-  public void afterTest() {
-  }
-
-}
